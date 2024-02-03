7f9de36328fd9eb83fa707767c8fcbd4cdfcdf5a
user <user@gmail.com>
2024-02-03 20:38:08 +0300

    fixed

--- script.js
+++ script.js
@@ -1,78 +1,78 @@
-let start=0;
-let end=0;
-let condition=true;
-function forward() {
-    anime({
-        targets: '.menu-small',
-        translateX: ['-100%', '0'],
-        easing: 'easeInOutQuad',
-        direction: 'alternate',
-        duration: 1000,
-        loop: false
-    });
-    condition=false; 
-    anime({
-        targets: '.stick',
-        rotate: 180,
-        easing: 'easeInOutQuad',
-        direction: 'alternate',
-        duration: 1000,
-        loop: false
-    });
-    anime({
-        targets: '.menu_small_icon',
-        rotate: 90,
-        easing: 'easeInOutQuad',
-        direction: 'alternate',
-        duration: 1000,
-        loop: false
-    });
-};
+// let start=0;
+// let end=0;
+// let condition=true;
+// function forward() {
+//     anime({
+//         targets: '.menu-small',
+//         translateX: ['-100%', '0'],
+//         easing: 'easeInOutQuad',
+//         direction: 'alternate',
+//         duration: 1000,
+//         loop: false
+//     });
+//     condition=false; 
+//     anime({
+//         targets: '.stick',
+//         rotate: 180,
+//         easing: 'easeInOutQuad',
+//         direction: 'alternate',
+//         duration: 1000,
+//         loop: false
+//     });
+//     anime({
+//         targets: '.menu_small_icon',
+//         rotate: 90,
+//         easing: 'easeInOutQuad',
+//         direction: 'alternate',
+//         duration: 1000,
+//         loop: false
+//     });
+// };
 
-let links=document.querySelectorAll('.scroll');
-let targeID;
-links.forEach(function (element){
-    element.addEventListener('click', function (event) {
-        event.preventDefault();
-        targetID=element.getAttribute('href');
-        document.querySelector(targetID).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-    })
-});
+// let links=document.querySelectorAll('.scroll');
+// let targetID;
+// links.forEach(function (element){
+//     element.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         targetID=element.getAttribute('href');
+//         document.querySelector(targetID).scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'start'
+//         })
+//     })
+// });
 
-$(document).ready(function () {
-  $('.info_punkts').bxSlider();
-});
+// $(document).ready(function () {
+//   $('.info_punkts').bxSlider();
+// });
 
-function backward() {
-    anime({
-        targets: '.menu-small',
-        translateX: ['0', '-100%'],
-        easing: 'easeInOutQuad',
-        direction: 'alternate',
-        duration: 1000,
-        loop: false
-    });
-    anime({
-        targets: '.stick',
-        rotate: 0,
-        easing: 'easeInOutQuad',
-        direction: 'alternate',
-        duration: 1000,
-        loop: false
-    });
-    anime({
-        targets: '.menu_small_icon',
-        rotate: -180,
-        easing: 'easeInOutQuad',
-        direction: 'alternate',
-        duration: 1000,
-        loop: false
-    });
-    condition=true;
-};
+// function backward() {
+//     anime({
+//         targets: '.menu-small',
+//         translateX: ['0', '-100%'],
+//         easing: 'easeInOutQuad',
+//         direction: 'alternate',
+//         duration: 1000,
+//         loop: false
+//     });
+//     anime({
+//         targets: '.stick',
+//         rotate: 0,
+//         easing: 'easeInOutQuad',
+//         direction: 'alternate',
+//         duration: 1000,
+//         loop: false
+//     });
+//     anime({
+//         targets: '.menu_small_icon',
+//         rotate: -180,
+//         easing: 'easeInOutQuad',
+//         direction: 'alternate',
+//         duration: 1000,
+//         loop: false
+//     });
+//     condition=true;
+// };
 // $('.menu_small_icon').click(function(){
 //     if (condition==true) {
 //         forward();
--- script_try.js
+++ script_try.js
@@ -11,13 +11,34 @@
 //  console.log(email: ${email});
 //  console.log(password: ${password});
 // });
+
+
+
+
 function handle() {
-    let inputs = document.querySelectorAll(".try");
+    alert('Registration completed')
+    let inputs = document.querySelectorAll("input");
     let input_values = [];
     for (let i = 0; i < inputs.length; i++) {
         input_values.push(inputs[i].value);
     }
     console.log(input_values);
 };
-let btn=document.querySelector('.submit');
+
+
+let btn = document.querySelector('.sub');
 btn.addEventListener('click', handle);
+
+
+// function handle() {
+//     alert('Форма отправлена!');
+//     let inputs = document.querySelectorAll("input");
+//     let input_values = [];
+//     for (let i = 0; i < inputs.length; i++) {
+//         input_values.push(inputs[i].value);
+//     }
+//     console.log(input_values);
+// }
+
+// let button = document.querySelector('.btn');
+// button.addEventListener('click', handle);
